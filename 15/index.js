10c2e0de1664d69f373a09866dac37edf56a81dd
Eleonora Nikerina <eleonora.radina@mail.ru>
2023-11-26 19:40:00 +0400

    add task 15

--- 15/index.js
+++ 15/index.js
@@ -2,3 +2,35 @@
 для ожидания выполнения других асинхронных операций, и возвращает результат выполнения.
 
 */
+
+function performAsyncOperation(delay) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(`Operation completed in ${delay / 1000} seconds`);
+      }, delay);
+    });
+  }
+  
+  async function asyncFunction() {
+    try {
+      console.log('Start of the asynchronous function');
+  
+      
+      const operationResult1 = await performAsyncOperation(2000);
+      console.log(operationResult1);
+  
+      const operationResult2 = await performAsyncOperation(1000);
+      console.log(operationResult2);
+  
+      console.log('End of the asynchronous function');
+  
+      return 'All asynchronous operations completed';
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  }
+  
+  asyncFunction()
+    .then(result => console.log('Result:', result))
+    .catch(error => console.error('Global error:', error));
+  
